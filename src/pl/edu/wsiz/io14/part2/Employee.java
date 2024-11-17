998c7aa24f317fdf3d551a4c75402c97ee5624c7
MTymowicz <mariusz.tymowicz@gmail.com>
2024-11-17 09:02:39 +0100

    2.3 done

--- src/pl/edu/wsiz/io14/part2/Employee.java
+++ src/pl/edu/wsiz/io14/part2/Employee.java
@@ -1,5 +1,7 @@
 package pl.edu.wsiz.io14.part2;
 
+import java.util.Scanner;
+
 public class Employee {
     String firstname;
     String lastname;
@@ -8,4 +10,21 @@ public class Employee {
     void print() {
         System.out.printf("%s %s %d", firstname, lastname, age);
     }
+
+    static Employee read() {
+        Scanner scanner = new Scanner(System.in);
+        Employee employee = new Employee();
+
+        System.out.print("Podaj imię:         ");
+        employee.firstname = scanner.next();
+
+        System.out.print("Podaj nazwisko:     ");
+        employee.lastname = scanner.next();
+
+        System.out.print("Podaj wiek:         ");
+        employee.age = scanner.nextByte();
+
+        return employee;
+    }
+
 }
--- src/pl/edu/wsiz/io14/part2/Program.java
+++ src/pl/edu/wsiz/io14/part2/Program.java
@@ -2,11 +2,13 @@ package pl.edu.wsiz.io14.part2;
 
 public class Program {
     public static void main(String[] args) {
-        Employee employee = new Employee();
+//        Employee employee = new Employee();
+//
+//        employee.firstname = "Jan";
+//        employee.lastname = "Kowalski";
+//        employee.age = 30;
 
-        employee.firstname = "Jan";
-        employee.lastname = "Kowalski";
-        employee.age = 30;
+        Employee employee = Employee.read();
 
         employee.print();
     }
