53a55cec3bed69baf41086dd673e67b09163f119
mtymowicz <test@wsiz.pl>
2024-11-30 13:06:24 +0100

    2.10 done (if-else)

--- src/pl/edu/wsiz/io14/part2/Program.java
+++ src/pl/edu/wsiz/io14/part2/Program.java
@@ -1,25 +1,43 @@
 package pl.edu.wsiz.io14.part2;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Company company = new Company();
 
-        company.add( new Employee("Jan", "Kowalski", (byte) 30));
-        company.add( new Employee("Anna", "Nowak", (byte) 27));
-        company.add( new Employee("Piotr", "Kowalczyk", (byte) 35));
-        company.add( new Employee("Piotr", "Kowalczyk", (byte) 35));
+        company.add(new Employee("Jan", "Kowalski", (byte) 30));
+        company.add(new Employee("Anna", "Nowak", (byte) 27));
+        company.add(new Employee("Piotr", "Kowalczyk", (byte) 35));
+        company.add(new Employee("Piotr", "Kowalczyk", (byte) 35));
 
-        company.printEmployees();
+        do {
+            System.out.println("\nLista operacji:\n");
+            System.out.println("1 - wypisz listę pracowników");
+            System.out.println("2 - dodaj pracownika");
+            System.out.println("3 - usuń pracownika");
+            System.out.println("9 - zakończ program\n");
+            System.out.print("Podaj numer operacji: ");
 
-        company.removeEmployee(1);
+            int operation = scanner.nextInt();
 
-        company.printEmployees();
+            if (operation == 1) {
+                company.printEmployees();
+            } else if (operation == 2) {
+                company.add();
+            } else if (operation == 3) {
+                System.out.print("Podaj numer pracownika do usunięcia: ");
+                int employeeIndex = scanner.nextInt();
+                company.removeEmployee(employeeIndex);
+            } else if (operation == 9) {
+                break;
+            } else {
+                System.out.println("Operacja o podanym numerze nie istnieje!");
+            }
+        }
+        while (true);
 
-        company.removeEmployee(2);
 
-        company.printEmployees();
-
-        company.removeEmployee(0);
-        company.removeEmployee(-2);
     }
 }
